963ce3883360d49ac9d455d89bf9d462cd38d92a
hopzygirl <user@blent>
2024-11-05 21:03:16 +0000

    Solved conflict

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 def __dfs_util(self, v, visited):
     visited.add(v)
     for nb in self.graph[v]:
         if nb not in visited:
             self.__dfs_util(nb, visited)
->>>>>>> dev
