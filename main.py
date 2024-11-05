dc96ebf5cb04aff9dc472bd76147b4fc408d6886
hopzygirl <user@blent>
2024-11-05 20:59:37 +0000

    Edited main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
- 
 class Graph:
     
     def __init__(self):
@@ -8,24 +6,13 @@ class Graph:
     def add_edge(self, u, v):
         self.graph[u].append(v)
  
-    def __dfs_util(self, v, visited):
+    def dfs_rec(self, v, visited):
         visited.add(v)
         print(v, end=' ')
         for neighbour in self.graph[v]:
             if neighbour not in visited:
-                self.__dfs_util(neighbour, visited)
+                self.dfs_rec(neighbour, visited)
 
     def dfs(self, v):
         visited = set()
-        self.__dfs_util(v, visited)
-        
-g = Graph()
-g.add_edge(0, 1)
-g.add_edge(0, 2)
-g.add_edge(1, 2)
-g.add_edge(2, 0)
-g.add_edge(2, 3)
-g.add_edge(3, 3)
- 
-print("DSF à partir du noeud 0 :")
-g.dfs(0)
+        self.dfs_rec(v, visited)
